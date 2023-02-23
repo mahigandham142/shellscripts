08297cbc83076f41ca3de2bebab8e075f5bdfad5
phani-rudra9 <53288879+phani-rudra9@users.noreply.github.com>
2023-02-23 12:58:23 +0530

    Update register_ami.sh

--- register_ami.sh
+++ register_ami.sh
@@ -1,7 +1,8 @@
 #To register ami
 
-!/bin/bash
-aws ec2 describe-instances | grep -i "instanceid" | cut -d ":" -f 2 | tr -d '"' | tr -d "," >> demo.txt
+#!/bin/bash
+NOW=$( date '+%F-%H-%M-%S' )
+aws ec2 describe-instances | grep -i "instanceid" | cut -d ":" -f 2 | tr -d '"' | tr -d "," > demo.txt
 for each_ami in $(cat demo.txt)
 do
   if which $each_ami &> /dev/null
@@ -9,12 +10,12 @@ do
           echo "Already $each_ami is created"
   else
           echo "$each_ami is creating....."
-           aws ec2 create-image --instance-id $each_ami --name $each_ami-$BUILD_NUMBER --description "An AMI for my server" --tag-specifications ResourceType=image,Tags="[{Key=createdby,Value=mahesh}]"
+           aws ec2 create-image --instance-id $each_ami --name $each_ami-$NOW --description "An AMI for my server $each_ami-$NOW" --tag-specifications ResourceType=image,Tags="[{Key=createdby,Value=mahesh}]"
           if [[ $? -eq 0 ]]
           then
-                  echo "Successfully created --$each_ami ami"
+                  echo "Successfully created --$each_ami-$NOW ami"
           else
-                  echo "unable to create --$each_ami"
+                  echo "unable to create --$each_ami-$NOW"
           fi
   fi
 done
